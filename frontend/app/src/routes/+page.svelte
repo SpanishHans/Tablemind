9a2f7cc8e4d15756b38acabe93e9aa2d9c97675d
SpanishHans <juanj.martinezg96@gmail.com>
2025-05-18 20:31:03 -0500

    se añade modo dev

--- frontend/app/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<h1 class="text-4xl font-bold text-blue-600 mb-4">Welcome to SvelteKit</h1>
-<p class="text-gray-700">
-  Visit <a href="https://svelte.dev/docs/kit" class="text-blue-500 underline hover:text-blue-700">svelte.dev/docs/kit</a> to read the documentation
-</p>
